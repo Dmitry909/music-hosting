0bd94e404a6579d6ac8a69f21948980f02e4e7bd
Dmitry07 <dmitry.terenich@gmail.com>
2024-02-09 15:47:05 +0300

    Add style checking and format main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use axum::{
-    routing::{get, post},
     http::StatusCode,
-    Json, Router,
     response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
 };
 use serde::{Deserialize, Serialize};
 
@@ -10,11 +10,12 @@ use serde::{Deserialize, Serialize};
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root)).route("/users", post(create_user));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-    
 }
 
 async fn root() -> &'static str {
