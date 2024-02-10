1c69a6cde6b9ab1990459dcb1fa78921e2e1d710
Dmitriy Terenichev <57761090+Dmitry909@users.noreply.github.com>
2024-02-10 21:02:39 +0300

    Revert "Make singup handler"

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
 use axum::{
-    extract::State,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, sync::Arc, sync::RwLock};
-
-#[derive(Default)]
-struct AppState {
-    users: HashMap<String, String>,
-}
-
-type SharedStateType = Arc<RwLock<AppState>>;
 
 #[tokio::main]
 async fn main() {
-    let shared_state = SharedStateType::default();
+    tracing_subscriber::fmt::init();
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/users", post(singup))
-        .with_state(shared_state);
+        .route("/users", post(create_user));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -32,27 +22,22 @@ async fn root() -> &'static str {
     "Hello, world!"
 }
 
-async fn singup(
-    State(state): State<SharedStateType>,
-    Json(payload): Json<SingupRequest>,
-) -> impl IntoResponse {
-    let users = &mut state.write().unwrap().users;
-    users.insert(payload.username.clone(), payload.password);
-
-    let response = SingupResponse {
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
         username: payload.username,
     };
 
-    (StatusCode::CREATED, Json(response))
+    (StatusCode::CREATED, Json(user))
 }
 
 #[derive(Deserialize)]
-struct SingupRequest {
+struct CreateUser {
     username: String,
-    password: String,
 }
 
 #[derive(Serialize)]
-struct SingupResponse {
+struct User {
+    id: u64,
     username: String,
 }
