3622629a603aa69a4220cef632cc4ce7d1062b52
Dmitry07 <dmitry.terenich@gmail.com>
2024-02-14 08:59:47 +0300

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -84,23 +84,95 @@ mod tests {
 
     #[tokio::test]
     async fn singup_user_different_bodies() {
-        let shared_state = SharedState::default();
-
-        let app = Router::new()
-            .route("/singup", post(singup))
-            .with_state(shared_state);
-
+        // TODO: make app and shared_state common, have to use not one_shot
         {
+            let shared_state = SharedState::default();
+            let app = Router::new()
+                .route("/singup", post(singup))
+                .with_state(shared_state);
             let response = app
                 .oneshot(make_post_request("/singup", Body::empty()))
                 .await
                 .unwrap();
+            // TODO: why BAD_REQUEST here? Must be UNPROCESSABLE_ENTITY.
             assert_eq!(response.status(), StatusCode::BAD_REQUEST);
 
             let body = response.into_body().collect().await.unwrap().to_bytes();
             // let json_body: Json<SingupResponse> = Json::from_bytes(&body).unwrap();
             // assert_eq!(json_body.0.username, "abc");
             assert_eq!(&body[..], b"Failed to parse the request body as JSON: EOF while parsing a value at line 1 column 0");
+        }
+
+        {
+            let shared_state = SharedState::default();
+            let app = Router::new()
+                .route("/singup", post(singup))
+                .with_state(shared_state);
+            let response = app
+                .oneshot(make_post_request(
+                    "/singup",
+                    Body::from("{\"username\": \"alex_no_password\"}"),
+                ))
+                .await
+                .unwrap();
+            assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+
+            let body = response.into_body().collect().await.unwrap().to_bytes();
+            assert_eq!(&body[..], b"Failed to deserialize the JSON body into the target type: missing field `password` at line 1 column 32");
+        }
+
+        {
+            let shared_state = SharedState::default();
+            let app = Router::new()
+                .route("/singup", post(singup))
+                .with_state(shared_state);
+            let response = app
+                .oneshot(make_post_request(
+                    "/singup",
+                    Body::from("{\"password\": \"no_username\"}"),
+                ))
+                .await
+                .unwrap();
+            assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+
+            let body = response.into_body().collect().await.unwrap().to_bytes();
+            assert_eq!(&body[..], b"Failed to deserialize the JSON body into the target type: missing field `username` at line 1 column 27");
+        }
+
+        {
+            let shared_state = SharedState::default();
+            let app = Router::new()
+                .route("/singup", post(singup))
+                .with_state(shared_state);
+            let response = app
+                .oneshot(make_post_request(
+                    "/singup",
+                    Body::from("{\"password\": \"no_username\"}"),
+                ))
+                .await
+                .unwrap();
+            assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+
+            let body = response.into_body().collect().await.unwrap().to_bytes();
+            assert_eq!(&body[..], b"Failed to deserialize the JSON body into the target type: missing field `username` at line 1 column 27");
+        }
+
+        {
+            let shared_state = SharedState::default();
+            let app = Router::new()
+                .route("/singup", post(singup))
+                .with_state(shared_state);
+            let response = app
+                .oneshot(make_post_request(
+                    "/singup",
+                    Body::from("{\"username\": \"alex\",\"password\": \"alex1990\"}"),
+                ))
+                .await
+                .unwrap();
+            assert_eq!(response.status(), StatusCode::CREATED);
+
+            let body = response.into_body().collect().await.unwrap().to_bytes();
+            assert_eq!(&body[..], b"{\"username\":\"alex\"}");
         }
 
         // {
