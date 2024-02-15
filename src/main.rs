753c799c4f46da5567aa8633cbc2ba25f93f38b4
Dmitry07 <dmitry.terenich@gmail.com>
2024-02-15 15:01:38 +0300

    Add delete_account handler

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use axum::{
+    body::Bytes,
     extract::State,
     http::StatusCode,
     response::IntoResponse,
-    routing::{get, post},
+    routing::{delete, get, post},
     Json, Router,
 };
-use ring::digest::{Context, Digest, SHA256};
+use hex;
 use serde::{Deserialize, Serialize};
-use std::str;
+use sha2::{Digest, Sha256};
 use std::{collections::HashMap, sync::Arc, sync::RwLock};
+use std::{io::Read, str};
 
 #[derive(Default)]
 struct AppState {
-    users: HashMap<String, Digest>,
+    users: HashMap<String, String>,
 }
 
 type SharedState = Arc<RwLock<AppState>>;
@@ -21,6 +23,7 @@ fn create_app() -> Router {
     let shared_state = SharedState::default();
     Router::new()
         .route("/singup", post(singup))
+        .route("/delete_account", delete(delete_account))
         .with_state(shared_state)
 }
 
@@ -32,12 +35,12 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-fn get_hash(username: &String, password: &String) -> Digest {
-    let mut context = Context::new(&SHA256);
-    context.update(username.as_bytes());
-    context.update(b"qIy074EXAsMI");
-    context.update(password.as_bytes());
-    context.finish()
+fn get_hash(password: &String) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(b"qIy074EXAsMI");
+    hasher.update(password.as_bytes());
+    let result = hasher.finalize();
+    hex::encode(result)
 }
 
 async fn singup(
@@ -50,7 +53,7 @@ async fn singup(
     }
     users.insert(
         input_payload.username.clone(),
-        get_hash(&input_payload.username, &input_payload.password),
+        get_hash(&input_payload.password),
     );
 
     let response = SingupResponse {
@@ -58,6 +61,28 @@ async fn singup(
     };
 
     (StatusCode::CREATED, Json(response)).into_response()
+}
+
+async fn delete_account(
+    State(state): State<SharedState>,
+    Json(input_payload): Json<DeleteAccountRequest>,
+) -> impl IntoResponse {
+    let users = &mut state.write().unwrap().users;
+    if !users.contains_key(&input_payload.username) {
+        return (StatusCode::NOT_FOUND, "Username doesn't exist").into_response();
+    }
+
+    let password_hash = get_hash(&input_payload.password);
+    if users[&input_payload.username] != password_hash {
+        return (StatusCode::FORBIDDEN, "Wrong password").into_response();
+    }
+
+    users.remove(&input_payload.username);
+    let response = DeleteAccountResponse {
+        username: input_payload.username,
+    };
+
+    (StatusCode::OK, Json(response)).into_response()
 }
 
 #[derive(Deserialize)]
@@ -68,6 +93,17 @@ struct SingupRequest {
 
 #[derive(Serialize, Deserialize)]
 struct SingupResponse {
+    username: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteAccountRequest {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DeleteAccountResponse {
     username: String,
 }
 
