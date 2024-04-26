ec35eb5fe94fdfa179448fa9f3b0b3cbdb7e1dfa
Dmitry07 <dmitry.terenich@gmail.com>
2024-04-26 17:41:16 +0300

    Add port as argument

--- main_service/src/main.rs
+++ main_service/src/main.rs
@@ -1,9 +1,18 @@
 use main_service;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: ./main_service [port]");
+        std::process::exit(1);
+    }
+    let port = &args[1];
+    let host = format!("0.0.0.0:{}", port);
+
     let app = main_service::create_app().await;
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3002").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(host).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
