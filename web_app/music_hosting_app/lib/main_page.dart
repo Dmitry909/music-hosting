702cc4943c0ddb3019f0e57701e3fbb117a22c95
Dmitry07 <dmitry.terenich@gmail.com>
2024-05-12 12:31:47 +0300

    Move upload track into a separate page

--- web_app/music_hosting_app/lib/main_page.dart
+++ web_app/music_hosting_app/lib/main_page.dart
@@ -1,15 +1,10 @@
-import 'dart:convert';
-// import 'dart:html';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:file_picker/file_picker.dart';
-import 'package:dio/dio.dart';
 
+import 'shared_state.dart';
 import 'signup_page.dart';
 import 'login_page.dart';
-import 'shared_state.dart';
+import 'upload_track.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -19,10 +14,6 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   bool _isTokenValid = false;
   String username = "USERNAME INITIAL VALUE";
-  TextEditingController _trackNameController = TextEditingController();
-  String _selectFileStatus = 'File not selected';
-  String _uploadTrackResult = '';
-  PlatformFile? _selectedFile;
 
   @override
   void initState() {
@@ -64,74 +55,6 @@ class _MainPageState extends State<MainPage> {
         MaterialPageRoute(
           builder: (context) => MainPage(),
         ));
-  }
-
-  Future<void> _selectFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
-    if (result != null) {
-      setState(() {
-        _selectedFile = result.files.single;
-        _selectFileStatus = 'Selected file: ${_selectedFile?.path}';
-      });
-    }
-  }
-
-  Future<void> _uploadTrack() async {
-    String trackName = _trackNameController.text;
-    if (trackName == "") {
-      setState(() {
-        _uploadTrackResult = "Name not specified";
-      });
-      return;
-    }
-
-    if (_selectedFile == null) {
-      setState(() {
-        _uploadTrackResult = "File not selected";
-      });
-      return;
-    }
-
-    String filename = _selectedFile?.name ?? "";
-    String pathToFile = _selectedFile?.path ?? "";
-    if (pathToFile == "") {
-      return;
-    }
-
-    final formData = FormData.fromMap({
-      "file": await MultipartFile.fromFile(pathToFile, filename: filename),
-      "track_name": trackName
-    });
-    final token = (await getToken())!;
-    final headers = {'authorization': token, "Content-Type": "multipart"};
-
-    try {
-      Response response = await Dio().post(
-        'http://localhost:3000/upload_track',
-        data: formData,
-        options: Options(headers: headers),
-      );
-
-      if (response.statusCode == 201) {
-        setState(() {
-          _uploadTrackResult = 'Track uploaded';
-          _selectFileStatus = 'File not selected';
-          _trackNameController.clear();
-        });
-      } else {
-        final statusCode = response.statusCode;
-        // final body = response.body;
-        // setState(() {_uploadTrackResult = 'Upload failed. response status code: $statusCode, body: $body';});
-        setState(() {
-          _uploadTrackResult =
-              'Upload failed. response status code: $statusCode';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _uploadTrackResult = 'Error occurred: $e. Please try again.';
-      });
-    }
   }
 
   @override
@@ -196,37 +119,14 @@ class _MainPageState extends State<MainPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _trackNameController,
-              decoration: InputDecoration(
-                labelText: 'Track name',
-              ),
-            ),
-            SizedBox(height: 16),
             ElevatedButton(
-              onPressed: _selectFile,
-              child: Text('Select File'),
-            ),
-            Text(
-              _selectFileStatus,
-              style: TextStyle(
-                color: _selectFileStatus == 'File not selected'
-                    ? Colors.red
-                    : Colors.green,
-              ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _uploadTrack,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => UploadTrackPage()),
+                );
+              },
               child: Text('Upload track'),
-            ),
-            Text(
-              _uploadTrackResult,
-              style: TextStyle(
-                color: _uploadTrackResult == 'Track uploaded'
-                    ? Colors.green
-                    : Colors.red,
-              ),
             ),
           ],
         ),
---
+++ web_app/music_hosting_app/lib/upload_track.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:dio/dio.dart';
+
+import 'shared_state.dart';
+import 'main_page.dart';
+
+class UploadTrackPage extends StatefulWidget {
+  @override
+  _UploadTrackPageState createState() => _UploadTrackPageState();
+}
+
+class _UploadTrackPageState extends State<UploadTrackPage> {
+  TextEditingController _trackNameController = TextEditingController();
+  String _selectFileStatus = 'File not selected';
+  String _uploadTrackResult = '';
+  PlatformFile? _selectedFile;
+
+  Future<void> _selectFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      setState(() {
+        _selectedFile = result.files.single;
+        _selectFileStatus = 'Selected file: ${_selectedFile?.path}';
+      });
+    }
+  }
+
+  Future<void> _uploadTrack() async {
+    String trackName = _trackNameController.text;
+    if (trackName == "") {
+      setState(() {
+        _uploadTrackResult = "Name not specified";
+      });
+      return;
+    }
+
+    if (_selectedFile == null) {
+      setState(() {
+        _uploadTrackResult = "File not selected";
+      });
+      return;
+    }
+
+    String filename = _selectedFile?.name ?? "";
+    String pathToFile = _selectedFile?.path ?? "";
+    if (pathToFile == "") {
+      return;
+    }
+
+    final formData = FormData.fromMap({
+      "file": await MultipartFile.fromFile(pathToFile, filename: filename),
+      "track_name": trackName
+    });
+    final token = (await getToken())!;
+    final headers = {'authorization': token, "Content-Type": "multipart"};
+
+    try {
+      Response response = await Dio().post(
+        'http://localhost:3000/upload_track',
+        data: formData,
+        options: Options(headers: headers),
+      );
+
+      if (response.statusCode == 201) {
+        setState(() {
+          _uploadTrackResult = 'Track uploaded';
+          _selectFileStatus = 'File not selected';
+          _trackNameController.clear();
+        });
+      } else {
+        final statusCode = response.statusCode;
+        // final body = response.body;
+        // setState(() {_uploadTrackResult = 'Upload failed. response status code: $statusCode, body: $body';});
+        setState(() {
+          _uploadTrackResult =
+              'Upload failed. response status code: $statusCode';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _uploadTrackResult = 'Error occurred: $e. Please try again.';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upload track page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _trackNameController,
+              decoration: InputDecoration(
+                labelText: 'Track name',
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _selectFile,
+              child: Text('Select File'),
+            ),
+            Text(
+              _selectFileStatus,
+              style: TextStyle(
+                color: _selectFileStatus == 'File not selected'
+                    ? Colors.red
+                    : Colors.green,
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _uploadTrack,
+              child: Text('Upload track'),
+            ),
+            Text(
+              _uploadTrackResult,
+              style: TextStyle(
+                color: _uploadTrackResult == 'Track uploaded'
+                    ? Colors.green
+                    : Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
