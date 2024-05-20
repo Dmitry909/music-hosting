2bcd4fdd5bd10f3f12bd5e87684a80d897526e1b
Dmitry07 <dmitry.terenich@gmail.com>
2024-05-20 20:07:03 +0300

    Add creating tracks directory

--- tracks_service/run.sh
+++ tracks_service/run.sh
@@ -3,9 +3,12 @@ sudo apt install -y postgresql postgresql-contrib postgresql-common postgresql-c
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
 sudo -u postgres psql -c "CREATE DATABASE tracks_service;"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'qwerty';"
 sudo -u postgres psql -d tracks_service -f migrations/0001_create_table.sql
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs < enter | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
+sudo apt install build-essential -y
+mkdir tracks
 cargo run -- 3002
