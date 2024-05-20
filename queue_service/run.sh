b034d58dd3b40343261b8860b4453a90752ccf7e
Dmitry07 <dmitry.terenich@gmail.com>
2024-05-20 20:39:42 +0300

    Add tracks host to queue

--- queue_service/run.sh
+++ queue_service/run.sh
@@ -2,4 +2,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
 sudo apt install build-essential -y
-cargo run -- 3004
+cargo run -- 3004 http://158.160.49.228:3002
