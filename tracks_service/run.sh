8271f9701464b5e87c522d13b6d74e3c8d233317
Dmitry07 <dmitry.terenich@gmail.com>
2024-05-20 17:23:33 +0300

    Remove repo from script

--- tracks_service/run.sh
+++ tracks_service/run.sh
@@ -1,8 +1,3 @@
-rm -rf ~/music-hosting
-sudo apt install git
-git clone https://github.com/Dmitry909/music-hosting.git
-cd music-hosting/tracks_service
-
 sudo apt install -y postgresql postgresql-contrib postgresql-common postgresql-client
 # sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
 sudo systemctl start postgresql
@@ -12,4 +7,5 @@ sudo -u postgres psql -d tracks_service -f migrations/0001_create_table.sql
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs < enter | sh -s -- -y
 source $HOME/.cargo/env
+
 cargo run -- 3002
